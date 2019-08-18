2929db8fd128e135d51f2a7f811f89bd774a7ff0
Henry Choi <wonderinghenry@gmail.com>
2019-08-18 20:48:02 +0900

    XAUUSD modified

--- processing/process.py
+++ processing/process.py
@@ -4,13 +4,12 @@ from processing import utils
 source = "../source/Prices.xlsx"
 
 dfs = pd.ExcelFile(source)
-print(dfs.sheet_names)
 
-df = dfs.parse(sheet_name="Daily_Indexed", skiprows=8, index_col=3)
+df = dfs.parse(sheet_name="Daily", skiprows=8, index_col=3)
 dfV2= df.drop(columns=['Unnamed: 0', 'Unnamed: 1', 'Unnamed: 2'])
 dfV2['Time'] = dfV2.index
 dfV3 = dfV2[['Time', 'US dollar', 'Euro', 'Korean won']]
 dfV3.index = list(range(10596))
-print(dfV3)
 dfV3.to_csv('../data/XAUUSD.csv')
 utils.pickle_object(dfV3, '../data/XAUUSD.pkl')
+print(dfV3)
